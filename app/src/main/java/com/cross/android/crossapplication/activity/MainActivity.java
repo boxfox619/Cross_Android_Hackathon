f887f6ae187870236abe80662fd5d5de21cd3d2d
dogyhbin2 <dogyhbin12345@gmail.com>
2018-06-01 21:11:25 +0900

    Main Recyclerview(이미지 처리 X), XML 거의 마무리

--- app/src/main/java/com/cross/android/crossapplication/activity/MainActivity.java
+++ app/src/main/java/com/cross/android/crossapplication/activity/MainActivity.java
@@ -1,18 +1,37 @@
 package com.cross.android.crossapplication.activity;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.widget.LinearLayout;
 
 import com.cross.android.crossapplication.R;
 
+import net.cachapa.expandablelayout.ExpandableLayout;
+
 public class MainActivity extends Activity {
+
+    LinearLayout main_linearlayout1 ;
+    ExpandableLayout main_topbox_linearlayout;
+    boolean topFlag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        main_linearlayout1 = findViewById(R.id.main_linearlayout1);
+        main_topbox_linearlayout = findViewById(R.id.main_topbox_linearlayout);
+
+        main_topbox_linearlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                main_topbox_linearlayout.setExpanded(true, true);
+            }
+        });
+
+
     }
+
+
 }
