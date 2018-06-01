d69cbc85b7c33513bda07cb76bfc20baee142a32
Boxfox <boxfoxsg619@gmail.com>
2018-06-01 22:16:17 +0900

    Implement the facebook login

--- app/src/main/java/com/cross/android/crossapplication/activity/MainActivity.java
+++ app/src/main/java/com/cross/android/crossapplication/activity/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 
 import com.cross.android.crossapplication.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/cross/android/crossapplication/activity/SplashActivity.java
@@ -0,0 +1,51 @@
+package com.cross.android.crossapplication.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.cross.android.crossapplication.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+public class SplashActivity extends AppCompatActivity {
+    private LoginButton loginButton;
+    private CallbackManager callbackManager = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        setContentView(R.layout.activity_splash);
+        loginButton = findViewById(R.id.btn_fb);
+        findViewById(R.id.btnLoginFacebook).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginButton.performClick();
+            }
+        });
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+            }
+
+            @Override
+            public void onCancel() {
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+            }
+        });
+    }
+}
